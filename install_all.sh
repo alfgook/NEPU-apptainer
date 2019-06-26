1ec7dc3a784368231f76e7e789d770738b91dafa
gschnabel <40870991+gschnabel@users.noreply.github.com>
2019-06-26 15:50:22 +0200

    added R repository

--- install_all.sh
+++ install_all.sh
@@ -61,13 +61,16 @@ apt install -yq sshpass
 apt install -yq rsync
 apt install -yq openssh-server
 
-apt install -yq r-base
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+echo "deb [ arch=amd64 ] https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/" | tee /etc/apt/sources.list.d/r-project-3.5.list
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 apt update
-apt install -y mongodb-org
 
+apt install -yq mongodb-org
+apt install -yq r-base
 
 ##################################################
 #       INSTALL REQUIRED R PACKAGES
