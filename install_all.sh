d2bebd005af1fc591f29c7d81ffccd23119314e8
Georg Schnabel <40870991+gschnabel@users.noreply.github.com>
2020-07-20 20:07:54 +0200

    fixed retrieval of signature key behind firewall

--- install_all.sh
+++ install_all.sh
@@ -73,7 +73,7 @@ apt install -yq openssh-server
 apt install -yq locales
 sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && locale-gen
 
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 echo "deb [ arch=amd64 ] https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/" | tee /etc/apt/sources.list.d/r-project-3.5.list
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
