cb54ef01547526972f2d6a963d29862fb52dbc18
Georg Schnabel <40870991+gschnabel@users.noreply.github.com>
2020-07-20 20:04:44 +0200

    fixed initialization of paths in config files

--- install_all.sh
+++ install_all.sh
@@ -203,12 +203,19 @@ chmod 600 "/home/$username/.ssh/authorized_keys"
 # download pipeline
 cd "/home/$username"
 git clone https://github.com/gschnabel/eval-fe56.git
-sed -i 's/ssh_login *<- *"[^"]*"/ssh_login <- "'"${username}"'@localhost"/' "eval-fe56/config.R"
-sed -i 's/calcdir_loc *<- *"[^"]*"/calcdir_loc <- "\/home\/'"$username"'\/calcdir"/' "eval-fe56/config.R"
-sed -i 's/calcdir_rem *<- *"[^"]*"/calcdir_rem <- "\/home\/'"$username"'\/remcalcdir"/' "eval-fe56/config.R"
-sed -i 's/rootpath *<- *"[^"]*"/rootpath <- "\/home\/'"$username"'\/eval-fe56"/' "eval-fe56/config.R"
-sed -i 's/savePathTalys *<- *"[^"]*"/savePathTalys <- "\/home\/'"$username"'\/talysResults"/' "eval-fe56/config.R"
-sed -i 's/\(initClusterTALYS(.*, *talysExe *= *"\)[^"]*\(",.*\)$/\1'"\\/home\\/$username\\/talys\\/source\\/talys"'\2/' "eval-fe56/config.R" 
+
+# function to update the paths in the config file
+update_config() {
+  sed -i 's/ssh_login *<- *"[^"]*"/ssh_login <- "'"${username}"'@localhost"/' "$1"
+  sed -i 's/calcdir_loc *<- *"[^"]*"/calcdir_loc <- "\/home\/'"$username"'\/calcdir"/' "$1"
+  sed -i 's/calcdir_rem *<- *"[^"]*"/calcdir_rem <- "\/home\/'"$username"'\/remcalcdir"/' "$1"
+  sed -i 's/rootpath *<- *"[^"]*"/rootpath <- "\/home\/'"$username"'\/eval-fe56"/' "$1"
+  sed -i 's/savePathTalys *<- *"[^"]*"/savePathTalys <- "\/home\/'"$username"'\/talysResults"/' "$1"
+  sed -i 's/\(initClusterTALYS(.*, *talysExe *= *"\)[^"]*\(",.*\)$/\1'"\\/home\\/$username\\/talys\\/source\\/talys"'\2/' "$1" 
+}
+# update paths in the config files
+update_config "eval-fe56/config.R"
+update_config "eval-fe56/config.R.fulleval"
 sed -i 's/setwd("[^"]*")/setwd("\/home\/'"$username"'\/eval-fe56")/' "eval-fe56/run_pipeline.R"
 
 # create an exemplary calculation directory
