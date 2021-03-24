dd1a02307610403383a766af3ee65be60396fb5e
gschnabel <40870991+gschnabel@users.noreply.github.com>
2021-03-24 11:51:51 +0100

    improved status output for TALYS install

--- 04_install_talys.sh
+++ 04_install_talys.sh
@@ -8,18 +8,16 @@ if [ ! -z "$talysurl" ]; then
     talys_tarfile=$(basename $talysurl)
     tar --no-same-owner -C "/home/$username/" -xf $talys_tarfile
     rm "$talys_tarfile"
+    echo Changing permissions, please be patient...
     chmod -R 777 "/home/$username/talys"
     find "/home/$username/talys" -type f -print0 | xargs -0 chmod 666
 
     cd "/home/$username/talys"
-    echo Changing permissions, please be patient...
     chmod 777 "talys.setup"
 
     echo Compiling talys executable...
     sed -i "s/compiler='gfortran'/compiler='gfortran -O2'/" talys.setup
     ./talys.setup
     echo The error about being unable to move talys to /root/bin is okay
-    echo Changing permissions, please be patient...
     chmod 777 "source/talys"
 fi
-
